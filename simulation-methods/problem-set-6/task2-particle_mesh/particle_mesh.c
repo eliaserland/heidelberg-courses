e08ba8a9e0ef50b04b99b978e674805f404de80a
Elias Olofsson <elias.olofsson@outlook.com>
2020-12-22 20:15:42 +0100

    FSM: Restructured sheet 6 folder a bit. Created skeleton for exercise 2.

---
+++ simulation-methods/problem-set-6/task2-particle_mesh/particle_mesh.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+#include <fftw3.h>
+
+#define M_PI 3.14159265358979323846
+
+#define PARTICLES 1
+
+/*
+ * 	Fundamentals of Simulation Methods - WiSe 2020/2021
+ * 	Problem Set 6 - Exercise 2
+ *
+ *   	Author: Elias Olofsson
+ * 	Date: 2020-12-22
+ */
+ 
+ int main(int argc, char **argv) {
+ 	
+ 	
+ 	return 0;
+ }
