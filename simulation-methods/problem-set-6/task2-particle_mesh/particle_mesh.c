12208f52cdf5955289ed8e4446f019d64bcb2874
Elias Olofsson <elias.olofsson@outlook.com>
2020-12-23 20:27:13 +0100

    FSM: Making some real progress on task 2, sheet 6.

--- simulation-methods/problem-set-6/task2-particle_mesh/particle_mesh.c
+++ simulation-methods/problem-set-6/task2-particle_mesh/particle_mesh.c
@@ -5,12 +5,12 @@
 #include <unistd.h>
 #include <math.h>
 #include <time.h>
-#include <fftw3.h>	// FFT
-#include <pcg_basic.h>	// RNG
+#include <fftw3.h>	// Fast Fourier Transform from http://www.fftw.org/ 
+#include <pcg_basic.h>	// Random Number Generator from https://www.pcg-random.org/
 
 #define M_PI 3.14159265358979323846
 
-#define DIM 2		// No. of dimensions
+#define DIM 2		// No. of dimensions (ONLY WORKING IN 2D FOR NOW)
 #define NGRID 256	// No. of gridpoints per dimension
 
 /*
@@ -21,7 +21,7 @@
  * 	Date: 2020-12-22
  */
  
-// Data type for storing all information for a single particle.
+/* Data type for storing all information for a single particle. */
 typedef struct {
  	double pos[DIM];
  	double vel[DIM];
@@ -31,11 +31,10 @@ typedef struct {
 } particle;
 
 
-// Return a random double from the flat distribution on [0,1).
+/* Return a random double from the flat distribution on [0,1). */
 double rand_double(pcg32_random_t *rngptr) {
 	return ldexp(pcg32_random_r(rngptr) ,-32);
 }	
-
 
 /** 
  * initialize() - Initialize the particles in the system.
@@ -92,57 +91,133 @@ void print_pos(particle *p, int NP) {
 	}
 }
 
+/** 
+ * fprint_density() - Print particle density field to a file.
+ * 
+ * @density: Pointer to the density field.
+ *
+ * Returns: Nothing.   
+ */
+void fprint_density(fftw_complex *density) {
+	
+	// Create an output file.
+	char fname[100];
+	sprintf(fname, "output_density.txt");
+	FILE *fd = fopen(fname, "w");
+	
+	for (int i = 0; i < pow(NGRID,DIM); i++) {
+		fprintf(fd, "%lf ", density[i][0]);	// Print the real part.
+		if (i%NGRID == NGRID-1) {
+			fprintf(fd, "\n");
+		}		
+	}
+}
 
-void calc_density(particle *p, double *density, int NP, double h) {
+/**
+ * calc_density() - Update the density field based on particle positions.
+ *
+ * @p: 	Pointer to array of particles.
+ * @density: 	Pointer to the density field.
+ * @NP: 	Number of particles.
+ * @h: 	One dimensional grid spacing.
+ *
+ * Returns: Nothing.
+ */
+void calc_density(particle *p, fftw_complex *density, int NP, double h) {
 	
 	// Volume of a single grid cell.
 	const double v = pow(h, DIM);
 	
 	// Set the entire density field to zero.
 	for (int i = 0; i < pow(NGRID, DIM); i++) {
-		density[i] = 0;
+		density[i][0] = 0; // Real part.
+		density[i][1] = 0; // Imaginary part.
 	}
 	
 	// Dynamic allocations.
-	double *W = malloc(pow(NGRID, DIM) * sizeof(double));
-	double *pos_int = malloc(DIM * sizeof(int));	// LOOK OVER
-	double *pos_star  = malloc(DIM*sizeof(double)) // LOOK OVER
-	 
-	// FLOATING POINT INDEX.
-	 
+	double *W = malloc(pow(2,DIM) * sizeof(double));  // Volume fraction
+	double *idx_float = malloc(DIM * sizeof(double)); // Floating point index
+	double *p_star    = malloc(DIM * sizeof(double)); // Length fraction 
+	
+	int idx_int[DIM]; 	// Lower corner index.
+	int idx[DIM];		// Current gridpoint index.
+	int lin_idx;		// Linear index of current gridpoint.
+	
 	// For each particle:
-	for (int i = 0; i < NP; i++) {
-		/* Get the grid point closest to the particle, and the volume 
-		   fraction of the grid cell in its lower corner. */
+	for (int i = 0; i < NP; i++) {		
+		// For each dimension:
 		for (int d = 0; d < DIM; d++) {
-			pos_int[d] = floor(p[i].pos[d]);  // WRONG! REDO
-			p_star[d] = pos_int[d] - p[i].pos[d]; // WRONG! REDO
+			// Calculate  floating point index of the particle.
+			idx_float[d] = p[i].pos[d]/h - 0.5;  
+
+			// Index of the gridpoint to lower side of the particle.
+			idx_int[d] = (int)floor(idx_float[d]);
+			
+			// Length fraction belonging to the gridpoint on the lower side.
+			p_star[d] = idx_float[d] - idx_int[d];
 		} 	
 		
+		// Set volume fractions to unity.
 		for (int j = 0; j < pow(2,DIM); j++) {
+			W[j] = 1; 
+		}
 		
-		
+		// For each of the two closest gridpoints in each dimension:
+		for (int j = 0; j < pow(2,DIM); j++) {
+			
+			/* Find the Cloud-in-Cell volume fraction W for the particle
+			   at the gridpoint. */
+			for (int d = 0; d < DIM; d++) {
+				if (((int)floor(j/pow(2,d)))%2 == 0) {
+					W[j] *= 1.0 - p_star[d];
+				} else {
+					W[j] *= p_star[d];
+				}
+			}
+			
+			/* Get the linear index of the gridpoint corresponding to
+			   the density field. */
+			lin_idx = 0;
+			for (int d = 0; d < DIM; d++) {
+				// Index of the gridpoint in the current dimension.
+				idx[d] = idx_int[d] + ((int)floor(j/pow(2,d)))%2;
+			
+				// Periodically map back points outside the grid.
+				if (idx[d] == -1) {
+					idx[d] += NGRID;
+				}
+				if (idx[d] == NGRID) {
+					idx[d] -= NGRID;
+				}
+				
+				/* Add contribution from current dimension to the
+				   linear index. */
+				lin_idx += idx[d] * pow(NGRID,d);
+			}
+
+			/* Assign to the density field, the mass fraction for the
+			   particle at the current gridpoint. */			
+			density[lin_idx][0] = p[i].m / v * W[j];  	// Real part.
+			density[lin_idx][1] = 0; 		 	// Imaginary part.
 		}
 	}
-	
+	// Free dynamically allocated memory.
 	free(W);
-	free(pos_int)
-	free(pos_star);
-	
+	free(idx_float);
+	free(p_star);
 }
 
 
 int main(int argc, char **argv) {
 
+	/* Simulation settings */
 	const int NP = 1;		// No. of particles to simulate.
 	const double L = 1; 		// Box side length.
 	
-	
-	
+	// -------------------------------------------------
 	const double h = L/NGRID; 	// Grid spacing. 
 	
-	
-	// Declare and seed the RNG.
+	// Declare and seed the RNG used for the particle initialization.
 	pcg32_random_t rngptr;
 	pcg32_srandom_r(&rngptr, time(NULL), (intptr_t)&rngptr);
 	
@@ -152,28 +227,58 @@ int main(int argc, char **argv) {
  	// Initialize the particles.
 	initialize(p, L, NP, &rngptr);
 
-	// Print position.
+	// Print the positions of the particles. (DEBUGGING)
 	print_pos(p, NP);
 
+	// Allocations.
+	fftw_complex *density_real   = fftw_malloc(pow(NGRID,DIM) * sizeof(fftw_complex));
+	fftw_complex *density_kspace = fftw_malloc(pow(NGRID,DIM) * sizeof(fftw_complex));
+	fftw_complex *greens_kspace  = fftw_malloc(pow(NGRID,DIM) * sizeof(fftw_complex));
+	fftw_complex *pot_real	      = fftw_malloc(pow(NGRID,DIM) * sizeof(fftw_complex));
 
-	// Allocate the density field.
-	double *density = malloc(pow(NGRID,DIM)*sizeof(double));
+	// Create FFTW plans for forward and backwards transformations.
+	fftw_plan plan_forward = fftw_plan_dft_2d(NGRID, NGRID, density_real, density_kspace,
+	 					   FFTW_FORWARD, FFTW_ESTIMATE);
+	fftw_plan plan_backward = fftw_plan_dft_2d (NGRID, NGRID, density_kspace, pot_real,
+						     FFTW_BACKWARD, FFTW_ESTIMATE);
+	
+	// Set up the Green's function in Fourier space.
+	for (int i = 0; i < pow(NGRID,DIM); i++) {
+		greens_kspace[i][0] = 0;	// DO SOMETHING THAT MAKES SENSE HERE
+		greens_kspace[i][1] = 0;	
+	}
 	
 	// Calulate the density field from the particle positions.
-	calc_density() 
-	 
+	calc_density(p, density_real, NP, h); 
+	
+	// Print density field to a file. (DEBUGGING)
+	fprint_density(density_real);
+	
+	// Forward Fourier transform of the density field. (2D ONLY HERE)
+	fftw_execute(plan_forward);
+	
+	
+	// Multiply density field with green's function in kspace.
+	
+	// Backwards Fourier transform to obtain the potential field.
+	fftw_execute(plan_backward);
+	
+	
+	// OTHER THINGS LATER
 	
 		
-	
-	
-	
-	
-	
-	
-		
-	// Free dynamically allocated memory.
+	// Free all dynamically allocated memory.
  	free(p);
- 	free(density);
+ 	
+ 	fftw_destroy_plan(plan_forward);
+	fftw_destroy_plan(plan_backward);
+ 	
+ 	fftw_free(density_real);
+ 	fftw_free(density_kspace);
+ 	fftw_free(greens_kspace);
+ 	fftw_free(pot_real);
+ 	
+ 	fftw_cleanup();
  	
  	return 0;
  }
